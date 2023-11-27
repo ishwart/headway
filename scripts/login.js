ccfa0513b48e47adf120156c621fe35c7b07ec11
Ishwar T <85565796+ishwart@users.noreply.github.com>
2023-11-27 22:29:44 +0530

    puah

--- scripts/login.js
+++ scripts/login.js
@@ -1,5 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(function () {
-        document.body.classList.add("content-loaded");
-    }, 1000);
+    const lastVisitTimestamp = localStorage.getItem("lastVisitTimestamp");
+
+    const is30MinutesSinceLastVisit = !lastVisitTimestamp || (Date.now() - lastVisitTimestamp > 30 * 60 * 1000);
+
+    if (is30MinutesSinceLastVisit) {
+        showLoadingPage();
+
+        setTimeout(function () {
+            hideLoadingPage();
+            localStorage.setItem("lastVisitTimestamp", Date.now());
+        }, 1000);
+    } else {
+        hideLoadingPage();
+    }
 });
+
+function showLoadingPage() {
+    document.body.classList.add("content-loaded");
+    document.getElementById("loadingPage").style.backgroundColor = 'transpe'; // Replace 'transpe' with the actual color or style
+
+    const loadingLogo = document.querySelector(".loading-logo");
+    loadingLogo.classList.add("jelly-animation"); // Add the class to enable the animation
+}
+
+function hideLoadingPage() {
+    const loadingPage = document.getElementById("loadingPage");
+    loadingPage.style.display = "none";
+}
