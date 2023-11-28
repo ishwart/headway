2d6e7aca6e317d1e35dc40d885d2b9650e9a0ee7
Ishwar T <85565796+ishwart@users.noreply.github.com>
2023-11-28 23:18:56 +0530

    pop filling form

--- scripts/login.js
+++ scripts/login.js
@@ -1,29 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const lastVisitTimestamp = localStorage.getItem("lastVisitTimestamp");
+    var hidebutton = document.getElementById("hidebutton");
+    var studentcontainer = document.getElementById("studentcontainer");
+    var logincontainer = document.getElementById("logincontainer");
 
-    const is30MinutesSinceLastVisit = !lastVisitTimestamp || (Date.now() - lastVisitTimestamp > 30 * 60 * 1000);
-
-    if (is30MinutesSinceLastVisit) {
-        showLoadingPage();
-
-        setTimeout(function () {
-            hideLoadingPage();
-            localStorage.setItem("lastVisitTimestamp", Date.now());
-        }, 1000);
-    } else {
-        hideLoadingPage();
-    }
+    hidebutton.addEventListener("click", function () {
+        studentcontainer.style.display = "flex";
+        logincontainer.style.display = "none";
+    });
 });
-
-function showLoadingPage() {
-    document.body.classList.add("content-loaded");
-    document.getElementById("loadingPage").style.backgroundColor = 'transpe'; // Replace 'transpe' with the actual color or style
-
-    const loadingLogo = document.querySelector(".loading-logo");
-    loadingLogo.classList.add("jelly-animation"); // Add the class to enable the animation
-}
-
-function hideLoadingPage() {
-    const loadingPage = document.getElementById("loadingPage");
-    loadingPage.style.display = "none";
-}
---
+++ scripts/register.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var hidebutton = document.getElementById("hidebutton");
+    var studentcontainer = document.getElementById("studentcontainer");
+    var registercontainer = document.getElementById("registercontainer");
+    var emailinput = document.getElementById("emailinput");
+    var passwordinput = document.getElementById("passwordinput");
+    var conpasswordinput = document.getElementById("conpasswordinput");
+    var selectrole = document.getElementById("selectrole");
+    var companycontainer = document.getElementById("companycontainer")
+
+    hidebutton.addEventListener("click", function () {
+        var selectedRole = selectrole.value;
+
+        if (selectedRole === "student") {
+            if (!isValidEmail(emailinput.value.trim())) {
+                alert("Invalid Email");
+                return;
+            }
+
+            if (passwordinput.value.trim() === "" || conpasswordinput.value.trim() === "" || conpasswordinput.value.trim() !== passwordinput.value.trim()) {
+                alert("Password Doesn't Match");
+                return;
+            }
+
+            studentcontainer.style.display = "flex";
+            registercontainer.style.display = "none";
+        }
+
+        else if (selectedRole === "company") {
+            if (passwordinput.value.trim() === "" || conpasswordinput.value.trim() === "" || conpasswordinput.value.trim() !== passwordinput.value.trim()) {
+                alert("Password Doesn't Match");
+                return;
+            }
+            companycontainer.style.display = "flex";
+            registercontainer.style.display = "none";
+
+        }
+
+        else {
+            alert("Please Select Your Role");
+        }
+    });
+
+    function isValidEmail(email) {
+        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+});
